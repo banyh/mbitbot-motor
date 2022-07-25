86a06f7205b02c6d2a226708e47090dc959f764b
Ping Chu Hung <banyhong@gmail.com>
2022-07-25 23:34:28 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,27 @@
-input.onButtonPressed(Button.A, function () {
-    mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 90)
-    mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 120)
-    strip.setPixelColor(3, neopixel.hsl(0, 0, 10))
-    strip.setPixelColor(0, neopixel.hsl(0, 0, 0))
-    strip.show()
-})
-input.onButtonPressed(Button.B, function () {
-    mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 60)
-    mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 90)
-    strip.setPixelColor(0, neopixel.hsl(0, 0, 10))
-    strip.setPixelColor(3, neopixel.hsl(0, 0, 0))
-    strip.show()
-})
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 90)
-    mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 90)
-    strip.clear()
-    strip.show()
-})
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P12, 4, NeoPixelMode.RGB)
-strip.clear()
-strip.show()
-mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 90)
-mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 90)
-basic.forever(function () {
-    if (input.lightLevel() > 128) {
-        mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 60)
-        mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 120)
+makerbit.onIrDatagram(function () {
+    if (makerbit.irDatagram() == "0x7D7DBE41") {
+        basic.showArrow(ArrowNames.West)
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 80)
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 110)
+    } else if (makerbit.irDatagram() == "0x7D7D7E81") {
+        basic.showArrow(ArrowNames.East)
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 70)
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 100)
+    } else if (makerbit.irDatagram() == "0x7D7DDE21") {
+        basic.showArrow(ArrowNames.North)
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 70)
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 110)
+    } else if (makerbit.irDatagram() == "0x7D7D3EC1") {
+        basic.showArrow(ArrowNames.South)
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 90)
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 90)
     } else {
         mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 90)
         mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 90)
+        basic.clearScreen()
     }
-    led.plotBarGraph(
-    input.lightLevel(),
-    255
-    )
 })
+makerbit.connectIrReceiver(DigitalPin.P8, IrProtocol.NEC)
+mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 90)
+mbitbot.move_servo_pin(mbitbot.SePin.Sepin2, 90)
+let speed = 0
